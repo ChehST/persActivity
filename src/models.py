d81dc8e5a978adf325a94ccbb2428ee56b0836a3
ChehST <chehstream@gmail.com>
2023-02-08 10:56:57 +0400

    Наброски для пакетов

--- src/models.py
+++ src/models.py
@@ -1,4 +1,16 @@
 
 
+# cap = open(*pcap)  with module for capfiles
+
 class Packet:
     pass
+
+# pac_var = Packet.parse(cap)
+
+class Activity(Packet, object):
+    def __init__(self):
+        self.src_addr = obj.Packet
+
+# act_var = Activity(pac_var).resolve
+>>> {'ip1':3000 pacs,'ipN+1':2400 pacs}
+>>>
